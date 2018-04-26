9251ad232fe1002a7c1800e44c3a1adccf64ab18
QuanQuan <iotwlw@126.com>
2018-04-26 12:53:31 +0800

    unicode & regular expression learning

--- tests/regular_expression.py
+++ tests/regular_expression.py
@@ -1,4 +1,4 @@
-
+# coding: utf-8
 
 import re
 str = "https://www.amazon.com/EnacFire-Reversible-PowerLine-Charging-Arranged/dp/B01F5AV0U8/ref=as_li_ss_tl?ie=UTF8&qid=1479697465&sr=8-1&keywords=reversible+usb+cable&linkCode=sl1&tag=learninginhan-20&linkId=b7de3c50f7d3e4d4be05de395e30c315"
@@ -10,5 +10,11 @@ num = 1
 
 if num > 1:
     print num
+# 发的萨芬
 
-#http://www.jb51.net/article/99453.htm
+rating = '\xe7\x94\xa8\xe6\x88\xb7\xe8\xaf\x84\xe7\xba\xa7: 3/5\n2 \xe6\x9d\xa1\xe8\xaf\x84\xe8\xae\xba'
+rating = rating.decode('utf-8')
+# rating = rating.replace(u"用户评级: ", "")
+review_num = re.search('\s(\d\.?\d?)/\d+\\n(\d+)', rating)
+review_num = review_num.groups()
+print review_num
