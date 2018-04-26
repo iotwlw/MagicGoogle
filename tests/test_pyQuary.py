5bcce6ddc80f3ebd3b8ae7d47f5330ee5f6d6525
QuanQuan <iotwlw@126.com>
2018-04-26 15:10:42 +0800

    test

--- tests/test_pyQuary.py
+++ tests/test_pyQuary.py
@@ -22,41 +22,29 @@ PROXIES = [{
 # Or MagicGoogle()
 mg = MagicGoogle(PROXIES)
 
-google2amazon_results = open('./Satellite TV Equipment-0 2018-04-11 17;37;43.html', 'r')
+google2amazon_results = open('./Connectors Adapters-11 2018-04-25 00;21;03.html', 'r')
 google2amazon_result = google2amazon_results.read()
-pq_content = mg.pq_html(google2amazon_result)
-aa = pq_content('h1').eq(0).text()
-if aa == '302 Moved':
-    print aa
-else:
-    print('-----------')
 
-# try:
-#     f = open("./text.html", 'wb')
-#     f.write(google2amazon_result)
-#     f.close()
-# except:
-#     print "------------"
-# pq_content = mg.pq_html(google2amazon_result)
-# for item in pq_content('div.g').items():
-#     result = {'title': item('h3.r>a').eq(0).text()}
-#     href = item('h3.r>a').eq(0).attr('href')
-#     rating = item('div.f.slp').eq(0).text()
-#     if href:
-#         url = mg.filter_link(href)
-#         result['url'] = url
-#     text = item('span.st').text()
-#     result['text'] = text
-#     pprint.pprint(result)
-#
-#
-# s = rating.encode('utf-8')
-# print s.strip('')
-# print s.lstrip('Rating: ')
-# print s.rstrip(' reviews')
-# print s.lstrip('Rating: ').rstrip(' reviews')
-# a = s.replace("\xc2\xa0", "").lstrip('Rating: ').rstrip(' reviews')
-# b = a.split('-')
-# print(a.split('-'))
-#
-# print b[0]
+pq_content = mg.pq_html(google2amazon_result)
+for item in pq_content('div.g').items():
+    result = {'title': item('h3.r>a').eq(0).text()}
+    href = item('h3.r>a').eq(0).attr('href')
+    rating = item('div.f.slp').eq(0).text()
+    if href:
+        url = mg.filter_link(href)
+        result['url'] = url
+    text = item('span.st').text()
+    result['text'] = text
+    pprint.pprint(result)
+
+
+s = rating.encode('utf-8')
+print s.strip('')
+print s.lstrip('Rating: ')
+print s.rstrip(' reviews')
+print s.lstrip('Rating: ').rstrip(' reviews')
+a = s.replace("\xc2\xa0", "").lstrip('Rating: ').rstrip(' reviews')
+b = a.split('-')
+print(a.split('-'))
+
+print b[0]
